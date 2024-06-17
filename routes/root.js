10af2d8d029f3ca0907c4fc32fb5af6f18c2247c
Merilax <merilaxreloaded@gmail.com>
2024-06-17 18:12:08 +0200

    modified:   routes/root.js

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,11 @@
 import * as express from 'express';
 const rootRouter = express.Router();
 
-rootRouter.get('health', (req, res) => {
+rootRouter.get('/', (req, res) => {
+    res.status(200).json({ ok: true, reason: "This is an API server." });
+});
+
+rootRouter.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
 
