18259341eeeb07a75c5d9573efe239caa24add93
Merilax <merilaxreloaded@gmail.com>
2024-06-17 17:20:57 +0200

    Making sure

--- routes/root.js
+++ routes/root.js
@@ -2,7 +2,7 @@ import * as express from 'express';
 const rootRouter = express.Router();
 
 rootRouter.get('health', (req, res) => {
-    res.status(200).json({ ok: true });
+    res.status(200).send('OK');
 });
 
 export default rootRouter;
